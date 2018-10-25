48d9a843b71ac89e8f7e35284771e4ff748a7fb5
Nick Shepherd <shepherdnick@hotmail.com>
2018-10-25 20:41:41 +0100

    Initial commit

---
+++ ClientForm.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace RestClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VerbComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResponseTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RequestBodyTextBox = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.chkDigest = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(450, 64);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(75, 23);
+            this.ExecuteButton.TabIndex = 2;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Location = new System.Drawing.Point(112, 64);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(317, 20);
+            this.UrlTextBox.TabIndex = 1;
+            this.UrlTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UrlTextBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL:";
+            // 
+            // VerbComboBox
+            // 
+            this.VerbComboBox.FormattingEnabled = true;
+            this.VerbComboBox.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "HEAD",
+            "OPTIONS"});
+            this.VerbComboBox.Location = new System.Drawing.Point(12, 64);
+            this.VerbComboBox.Name = "VerbComboBox";
+            this.VerbComboBox.Size = new System.Drawing.Size(92, 21);
+            this.VerbComboBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Verb:";
+            // 
+            // ResponseTextBox
+            // 
+            this.ResponseTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResponseTextBox.Location = new System.Drawing.Point(12, 282);
+            this.ResponseTextBox.Multiline = true;
+            this.ResponseTextBox.Name = "ResponseTextBox";
+            this.ResponseTextBox.ReadOnly = true;
+            this.ResponseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ResponseTextBox.Size = new System.Drawing.Size(509, 270);
+            this.ResponseTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Response:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Request Body (optional):";
+            // 
+            // RequestBodyTextBox
+            // 
+            this.RequestBodyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RequestBodyTextBox.Location = new System.Drawing.Point(12, 150);
+            this.RequestBodyTextBox.Multiline = true;
+            this.RequestBodyTextBox.Name = "RequestBodyTextBox";
+            this.RequestBodyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.RequestBodyTextBox.Size = new System.Drawing.Size(509, 113);
+            this.RequestBodyTextBox.TabIndex = 7;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 25);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 20);
+            this.txtUsername.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(229, 25);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 20);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Username";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(234, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 40);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Request Content-Type";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(271, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(214, 17);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Other";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(112, 17);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(96, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "application/xml";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(100, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "application/json";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // chkDigest
+            // 
+            this.chkDigest.AutoSize = true;
+            this.chkDigest.Location = new System.Drawing.Point(450, 27);
+            this.chkDigest.Name = "chkDigest";
+            this.chkDigest.Size = new System.Drawing.Size(62, 17);
+            this.chkDigest.TabIndex = 14;
+            this.chkDigest.Text = "Digest?";
+            this.chkDigest.UseVisualStyleBackColor = true;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 564);
+            this.Controls.Add(this.chkDigest);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RequestBodyTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ResponseTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.VerbComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.ExecuteButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "ClientForm";
+            this.Text = "Rest Client";
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox VerbComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ResponseTextBox;
+        private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox RequestBodyTextBox;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox chkDigest;
+    }
+}
+
---
+++ ClientForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestClient
+{
+    public partial class ClientForm : Form
+    {
+        #region Class variables
+
+        HttpWebRequest _request = null;
+        CookieContainer _cookieContainer = new CookieContainer();
+        string _method = string.Empty;
+
+        #endregion
+
+        private void MakeRequest()
+		{
+			ResponseTextBox.Text = "Please wait...";
+
+			var url = UrlTextBox.Text;
+			_method = VerbComboBox.Text;
+			var requestBody = RequestBodyTextBox.Text;
+
+			try
+			{
+                // Create the request object
+				_request = (HttpWebRequest)WebRequest.Create(url);
+                //_request.Credentials = new NetworkCredential(txtUsername.Text, txtPassword.Text);
+                //_request.Headers.Add(HttpRequestHeader.Authorization, string.Format("Basic {0}", Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", txtUsername.Text, txtPassword.Text)))));
+                //_request.PreAuthenticate = true;
+                //_request.ContentLength = RequestBodyTextBox.Text.Length;
+                _request.Method = _method;
+                //_request.CookieContainer = _cookieContainer;
+
+                // Customizable strings
+                //_request.UserAgent = "This is a generic user-agent";
+                //_request.Headers.Add("CUSTOM_HEADER", "HI, HOW ARE YOU?");
+                _request.Headers.Add("Authorization", "Bearer 89EF8594E2EA6756BAC84D26D75F8");
+
+                // Change the content type if we're using something other than GET
+                if (_method != "GET")
+                {
+                    if (radioButton1.Checked)
+                    {
+                        _request.ContentType = "application/json";
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        _request.ContentType = "application/xml";
+                    }
+                    else if (radioButton3.Checked) { 
+                        _request.ContentType = textBox3.Text;
+                    }
+                }
+                
+                // Set the request body to a block of data
+				SetBody(_request, requestBody);
+
+                try
+                {
+                    // Try and get a response, if we don't then just process the event
+                    var response = (HttpWebResponse)_request.GetResponse();
+                    ProcessResponse(response);
+                }
+                catch(WebException wex)
+                {
+                    // If we're doing digest auth, then we expect a 401 (Unauthorized)
+                    if(wex.Response != null && ((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        ProcessResponse(wex.Response);
+                    }
+                    else
+                    {
+                        ResponseTextBox.Text += "ERROR: " + wex.Message;
+                    }
+                }
+			}
+			catch (Exception ex)
+			{
+				ResponseTextBox.Text += "ERROR: " + ex.Message;
+			}
+		}
+
+        private void ProcessResponse(WebResponse response)
+        {
+            // Case to a better response objects
+            HttpWebResponse upgradedResponse = (HttpWebResponse)response;
+
+            // If we've received a 401, we're trying to do digest auth
+            if (upgradedResponse.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                // Get the authentication header which contains all the headers we need to reply with
+                var wwwAuthenticateHeader = upgradedResponse.Headers["WWW-Authenticate"];
+
+                // Turn the request uri into a uri object so we can use it later
+                Uri uri = new Uri(_request.RequestUri.ToString());
+
+                // Build the lookahead (?= regular expression (order doesn't matter)
+                var regularExpression = "(?=.*realm=\"(?<realm>.*?)\") " + 
+                    "(?=.*nonce=\"(?<nonce>.*?)\")" + 
+                    "(?=.*opaque=\"(?<opaque>.*?)\")" + 
+                    "(?=.*algorithm=(?<algorithm>.*?),)" + 
+                    "(?=.*qop=\"(?<qop>.*?)\")";
+
+                Match match = Regex.Match(wwwAuthenticateHeader, regularExpression);
+                if (match.Success)
+                {
+                    // If we got all of those paramters from the header, grab them as variables
+                    string realm = match.Groups["realm"].Value;
+                    string nonce = match.Groups["nonce"].Value;
+                    string opaque = match.Groups["opaque"].Value;
+                    string qop = match.Groups["qop"].Value;
+                    string algorithm = match.Groups["algorithm"].Value;
+
+                    // Generate the encrypted authentication string
+                    string authResp = GenerateAuthResponse(txtUsername.Text, realm, txtPassword.Text, _method.ToString(), uri.PathAndQuery, nonce);
+
+                    // Build the auth string to use as a header for the second request
+                    string authString = string.Format("Digest username=\"{0}\", realm=\"{1}\", nonce=\"{2}\", uri=\"{3}\", response=\"{4}\"",
+                        txtUsername.Text, realm, nonce, uri.PathAndQuery, authResp);
+
+                    // Create the second request
+                    var secondRequest = (HttpWebRequest)WebRequest.Create(_request.RequestUri.ToString());
+                    secondRequest.Headers.Add("Authorization", authString); // add our encrypted auth string
+                    secondRequest.CookieContainer = _cookieContainer; // add the same bunch of cookies we sent with the original request
+
+                    // Try a second request to authenticate this time
+                    var secondResponse = (HttpWebResponse)secondRequest.GetResponse();
+                    ResponseTextBox.Text = ConvertResponseToString(secondResponse);
+                }
+            }
+            else if(upgradedResponse.StatusCode == HttpStatusCode.OK)
+            {
+                // If we just did a normal non-digest request then just process the response
+                ResponseTextBox.Text = ConvertResponseToString(upgradedResponse);
+            }
+        }
+
+        string GenerateAuthResponse(string user, string realm, string pass, string command, string uri, string nonce)
+        {
+            string hash1 = string.Empty;
+            string hash2 = string.Empty;
+
+            using (MD5 hashObject = MD5.Create())
+            {
+                hash1 = GetMd5Hash(hashObject, String.Format("{0}:{1}:{2}", user, realm, pass));
+                hash2 = GetMd5Hash(hashObject, String.Format("{0}:{1}", command, uri));
+                return GetMd5Hash(hashObject, string.Format("{0}:{1}:{2}", hash1, nonce, hash2));
+            }
+        }
+
+        string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+		void SetBody(HttpWebRequest request, string requestBody)
+		{
+			if (requestBody.Length > 0)
+			{
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    using (StreamWriter writer = new StreamWriter(requestStream))
+                    {
+                        writer.Write(requestBody);
+                    }
+                }
+			}
+		}
+
+		string ConvertResponseToString(HttpWebResponse response)
+		{
+			string result = "Status code: " + (int)response.StatusCode + " " + response.StatusCode + "\r\n";
+
+			foreach (string key in response.Headers.Keys)
+			{
+				result += string.Format("{0}: {1} \r\n", key, response.Headers[key]);
+			}
+
+			result += "\r\n";
+			result += new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+			return result;
+		}
+
+		public ClientForm()
+		{
+			InitializeComponent();
+		}
+
+		void ClientForm_Load(object sender, EventArgs e)
+		{
+			VerbComboBox.SelectedIndex = 0;
+			UrlTextBox.Text = "http://localhost/";
+		}
+
+		void ExecuteButton_Click(object sender, EventArgs e)
+		{
+			MakeRequest();
+		}
+
+		void UrlTextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				MakeRequest();
+				e.Handled = true;
+			}
+		}
+	}
+}
